47f2b64f656fcc9b21f28f14fa7f563aa0e70166
AzizbekFlutterDeveloper <xxuliganov7@gmail.com>
2022-02-17 12:34:29 -0500

    tezroq_tugat

--- lib/core/phone_namber.dart
+++ lib/core/phone_namber.dart
@@ -1,5 +1,49 @@
-class SendCode{
-  static sendSms(){
-    
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:git/screens/home.dart';
+
+class SendCode {
+  static final _firebaseInit = Firebase.initializeApp();
+  static FirebaseAuth _authUser = FirebaseAuth.instance;
+
+  static TextEditingController phoneController = TextEditingController();
+  static TextEditingController smsController = TextEditingController();
+  static Future sendSms(context) async {
+    _authUser.verifyPhoneNumber(
+      phoneNumber: phoneController.text,
+      verificationCompleted: (v) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Verification is completed !!!"),
+          ),
+        );
+      },
+      verificationFailed: (v) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Verifiction Faild !!!"),
+          ),
+         
+        );
+        print("$v");
+      },
+      codeSent: (verificationId, resentToken) async {
+        PhoneAuthCredential _phoneCredential = PhoneAuthProvider.credential(
+          verificationId: verificationId,
+          smsCode: smsController.text,
+        );
+
+        await _authUser.signInWithCredential(_phoneCredential).then((value) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const HomePage(),
+            ),
+          );
+        });
+      },
+      codeAutoRetrievalTimeout: (v) {},
+    );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:git/core/phone_namber.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -6,21 +7,30 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.05,
-          width: MediaQuery.of(context).size.width * 0.7,
-          child: TextFormField(
-            decoration: InputDecoration(
-              hintText: "Phone Number",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              )
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+            width: MediaQuery.of(context).size.width * 0.7,
+            child: TextFormField(
+              controller: SendCode.phoneController,
+              decoration: const InputDecoration(
+                  hintText: "Phone Number",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  )),
             ),
           ),
-        ),
-      ],
-    ));
+          ElevatedButton(
+            child: Text("Next"),
+            onPressed: () {
+              SendCode.sendSms(context);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
